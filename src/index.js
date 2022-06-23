befd142cc8aa8b0943b5fb02dd82b0d2e439b2f3
Slava Ivanov <drumtrack@ya.ru>
2022-06-23 18:48:54 +0300

    upd: resolved toReadable() function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function toReadable (number) {
-  
+    if (!number) return "zero";
+    const first = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    const units = [...`${number}`].map(e => +e).reverse();
+    const end = first[+`${units[1]}${units[0]}`] ?? `${tens[units[1]] ?? ""} ${first[units[0]]}`;
+    return `${units[2] ? `${first[units[2]]} hundred ` : ""}${end}`.trim();
 }
