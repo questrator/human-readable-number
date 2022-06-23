859f8a783b64a4f7369d114ffbd0646b3319ecb4
questrator <drumtrack@ya.ru>
2022-06-24 00:13:30 +0300

    upd: index.js performance

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function toReadable (number) {
     if (!number) return "zero";
     const first = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
     const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-    const units = [...`${number}`].map(e => +e).reverse();
-    const end = first[+`${units[1]}${units[0]}`] ?? `${tens[units[1]] ?? ""} ${first[units[0]]}`;
-    return `${units[2] ? `${first[units[2]]} hundred ` : ""}${end}`.trim();
+    const units = number.toString().split("").reverse();
+    const end = first[+`${units[1]}${units[0]}`] ?? `${tens[units[1]] ?? ""}` + ((!!+units[1] && !!+units[0]) ? " " : "") + `${first[units[0]]}`;
+    return `${units[2] ? `${first[units[2]]} hundred` + (end ? " " : "") : ""}${end}`;
 }
